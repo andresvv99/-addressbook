473756ed33b7d43631cecd6852861f54e1545237
andresvv99 <andres.viorels99@gmail.com>
2019-08-31 09:52:40 -0400

    webapiok

---
+++ MVC-address/apiAddressbook/Models/Book.cs
@@ -0,0 +1,26 @@
+﻿namespace apiAddressbook.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public enum TypeContact
+    {
+        Correo,
+        Telefono,
+        Facebook
+    }
+    public class Book
+    {
+        [Key]
+        public int BookID { get; set; }
+        [Required]
+        [Display(Name = "Nombre")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Tipo")]
+        public TypeContact Type { get; set; }
+        [Required]
+        public string Contact { get; set; }
+
+
+    }
+}
